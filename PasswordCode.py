bf9dfc2c01a91720184161a07e379c0181b7bc40
NastyDagger22 <87157291+NastyDagger22@users.noreply.github.com>
2022-09-21 21:06:08 +0800

    Add files via upload

---
+++ PasswordCode.py
@@ -0,0 +1,11 @@
+while True:
+    password = input("What is the password?: ")
+
+    if password == "password":
+        print(" ")
+        print("Access granted.")
+        print("Secret: I like dogs over cats.")
+        print(" ")
+
+    else:
+        print("Incorrect password, please try again.")
